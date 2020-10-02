4ca8f3f04f25ebbf212aee1cb5483a3d13bd683d
Tina May <tina@instituteofcode.com>
2020-10-02 15:37:13 +0800

    simple changes

--- assets/main.js
+++ assets/main.js
@@ -11,24 +11,20 @@ $(document).ready(function() {
 	});
 	// end hamburger menu
 
-	// This is the pop-up gallery
-	$('.popup-gallery').magnificPopup({
-		delegate: 'a',
-		type: 'image',
-		tLoading: 'Loading image #%curr%...',
-		mainClass: 'mfp-img-mobile',
-		gallery: {
-			enabled: true,
-			navigateByImgClick: true,
-			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
-		},
-		image: {
-			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-			titleSrc: function(item) {
-				return item.el.attr('title');
-			}
-		}
-	});
+	$(document).ready(function() {
+
+    // This is the pop-up gallery
+    $('[data-fancybox="gallery"]').fancybox({
+      protect: true,
+      keyboard: true,
+      arrows: true,
+      infobar: false,
+      buttons: [
+      "close"
+      ],
+    });
+
+  });
 
 	var allPanels = $('.accordion > dd').hide();
 
